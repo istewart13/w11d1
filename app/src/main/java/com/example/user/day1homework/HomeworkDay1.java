1f876469da5df5d10948d4341ccc7356f6fc2e67
Iain Stewart <iain.stewart08@gmail.com>
2016-08-16 09:17:26 +0100

    initial commit

---
+++ app/src/main/java/com/example/user/day1homework/HomeworkDay1.java
@@ -0,0 +1,16 @@
+package com.example.user.day1homework;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 15/08/2016.
+ */
+public class HomeworkDay1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
